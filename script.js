bc2463e9c6a2eac14b479871ccbcee5eff577111
unknown <Greg>
2021-05-11 01:29:18 +0200

    three

---
+++ script.js
@@ -0,0 +1,23 @@
+const click = document.getElementById('click');
+const dont_click = document.getElementById('dont_click');
+const modal_success = document.getElementById('success');
+const modal_error = document.getElementById('error');
+const close_success = document.getElementById('close_success');
+const close_error = document.getElementById('close_error');
+
+click.addEventListener('click', () => {
+    modal_success.classList.add('show')
+})
+
+dont_click.addEventListener('click', () => {
+    modal_error.classList.add('show')
+})
+
+close_success.addEventListener('click', () => {
+    modal_success.classList.remove('show')
+})
+
+close_error.addEventListener('click', () => {
+    modal_error.classList.remove('show')
+})
+
