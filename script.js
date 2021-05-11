1ffe8dcf13a227dd50f8c54d2fece3805783e5ad
unknown <Greg>
2021-05-11 02:11:08 +0200

    four

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ const modal_success = document.getElementById('success');
 const modal_error = document.getElementById('error');
 const close_success = document.getElementById('close_success');
 const close_error = document.getElementById('close_error');
+const value = document.getElementById('value')
+
+
+
 
 click.addEventListener('click', () => {
+    document.getElementById('result').innerHTML = "&nbsp"+document.getElementById('value').value+"&nbsp";
     modal_success.classList.add('show')
 })
 
